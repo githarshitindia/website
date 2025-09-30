b1ee545735f3d0afd0960a30e8e0d1c83443c334
githarshitindia <git.harshit09@gmail.com>
2025-09-30 11:23:32 +0530

    Create tailwind.config.js

---
+++ projects/tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
+            colors: {
+                'primary': '#4338ca', // Deep Indigo (700)
+                'primary-light': '#6366f1', // Lighter Indigo (500)
+            }
+        }
+    },
+    darkMode: 'class', 
+}
